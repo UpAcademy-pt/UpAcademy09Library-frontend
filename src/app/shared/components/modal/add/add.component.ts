78934b923a4f4abf7903e24b25bebdb2515e06e2
ffabiollopes <ffabiollopes@gmail.com>
2018-12-27 19:54:44 +0000

    add works

--- src/app/shared/components/modal/add/add.component.ts
+++ src/app/shared/components/modal/add/add.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
-import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
-import { CatalogApiService } from 'src/app/shared/services';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { DataService } from 'src/app/shared/services';
+import { Catalog } from 'src/app/shared/models';
 
 @Component({
   selector: 'app-add',
@@ -10,45 +11,19 @@ import { CatalogApiService } from 'src/app/shared/services';
 })
 export class AddComponent implements OnInit {
 
-bookForm: FormGroup;
-book_title:string='';
-book_author:string='';
-book_description:string=''
-book_isbn:number=null;
-isLoadingResults = false;
+  modalRef: BsModalRef;
+  catalog: Catalog = new Catalog();
 
-  
-closeResult: string;
+  constructor(private modalService: BsModalService, private dataService: DataService) { }
 
-  constructor(private modalService: NgbModal, private catalogApi: CatalogApiService, private formBuilder: FormBuilder, ) { }
+  ngOnInit() {}
 
-  ngOnInit() {
-
-    this.bookForm = this.formBuilder.group({
-      'book_title' : [null],
-      'book_author' : [null],
-      'book_description' : [null],
-      'book_isbn' : [null]
-    });
-    
-  }
-  open(content) {
-    
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
+  onSubmit() {
+  console.log(this.catalog);
+  this.dataService.createCatalog(this.catalog);
   }
-
-}
+ }
--- src/app/shared/models/catalog.ts
+++ src/app/shared/models/catalog.ts
@@ -1,20 +1,11 @@
 export class Catalog {
-    'title': string;
-    'description': string;
-    'author': string;
-    'isbn': string;
+    //ver porque do ?
+    id?: number;
+    title: string;
+    description: string;
+    author: string;
+    isbn: string;
     constructor(catalog?: any) {
         Object.assign(this, catalog);
     }
 }
-//db java
-// private String title;
-// private String description;
-// private String author;
-// private String photoLink;
-// private String topic;
-// private String location;
-// private String criationDate;
-// private String lastChangeDate;
-// private String isbn;
-// private String state;
