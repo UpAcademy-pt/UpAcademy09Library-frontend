9886c4ef1096534fd42a59768cf1bb376eda1ad7
jpvhcavalheiro <jpvhcavalheiro@gmail.com>
2019-01-03 14:15:04 +0000

    login utilizador não sei se funciona

--- src/app/layout/login/login.component.ts
+++ src/app/layout/login/login.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public account: Account = new Account();
+
+  constructor(
+    public router: Router,
+    private accountApi: AccountApiService
+  ) { }
 
   ngOnInit() {
   }
