08af460462285200a9f380bbcd3aac9a04c15a92
jpvhcavalheiro <jpvhcavalheiro@gmail.com>
2019-01-04 15:59:45 +0000

    cria registo

--- src/app/layout/register/register.component.ts
+++ src/app/layout/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/models/user';
+import { DataService } from 'src/app/shared/services';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User();
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
   }
+
+  onSubmit() {
+    console.log(this.user);
+    this.dataService.createUser(this.user);
+    }
 
 }
