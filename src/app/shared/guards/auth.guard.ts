c5405c170f4a93058ab84c6b6636155d73890289
Daniela Palma <palma.dmlc@gmail.com>
2019-01-07 17:08:41 +0000

    guards

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AcountApiService } from '../services/account/account-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+
+  constructor(
+    private router: Router,
+    private accountApi: AcountApiService
+  ) { }
+
+  canActivate() {
+    //   if (this.accountApi.isAuthenticated() && this.accountApi.isAdmin()) {
+    //     this.router.navigate(['/admin']);
+    //     return true;
+    //   } else if (this.accountApi.isAuthenticated() && !this.accountApi.isAdmin()) {
+    //     this.router.navigate(['/user']);
+    //     return true;
+    //   } else {
+    //     this.router.navigate(['/home']);
+    //   }
+    if (this.accountApi.isAuthenticated()) {
+      return true;
+    } else {
+      this.router.navigate(['/home']);
+    }
   }
 }
